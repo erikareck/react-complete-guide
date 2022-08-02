39f75c74bdb1753b9aa77f30e7d12b64de81667f
erikareck <erikareck2016@gmail.com>
2022-08-02 20:58:10 +0800

    init

---
+++ src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+//React uses Camel Sytle
+const ExpenseItem = (props) => {
+ 
+    const [title, setTitle] = useState(props.title);
+    console.log('ExpenseItem evaluated by React: ' + props.title);
+
+    const clickHandler = () => {
+      setTitle('Updated!');
+      console.log('title: ' + title);
+    }
+
+  /*{react only allow return only one root element}
+    you can use <div></div> around all the elements you want to return to follow this rule
+    */
+  return (
+    /*
+    The following html alike code is not html, it's 'javascript'
+    Therefore, we don't use 'class=""' in <div> but 'className=""'
+    */
+   /*
+   you can run basic javascript expression within the browser {}
+   */
+    <Card className="expense-item">
+    <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
